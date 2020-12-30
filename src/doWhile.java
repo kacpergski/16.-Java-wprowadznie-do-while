fdb3a27e76fd92ac132329031996f3c356cf9b63
Kacper Grzybkowski <kacperg@gmail.com>
2020-12-31 00:12:37 +0100

    feature-2 done

---
+++ src/doWhile.java
@@ -0,0 +1,22 @@
+public class doWhile {
+
+    public static void main(String[] args) {
+
+    int a = 7; int b;
+
+        System.out.println("------------FEATURE-2");
+        System.out.println();
+do {
+
+    b = a % 2;
+    a = (a / 2);
+    System.out.print(b);
+
+}while  (a!=0);
+        System.out.println();
+        System.out.println();
+        System.out.println("------------FEATURE-3");
+
+
+}
+}
